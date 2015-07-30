5da201227dd207c02e86d900ec33ccea9be88b45
Casey1203 <510471247@qq.com>
2015-07-30 17:13:14 +0800

    new WS4 not done yet

---
+++ WS4/src/Workshop4.java
@@ -0,0 +1,26 @@
+public class Workshop4 {
+
+   /**
+	 * Task 1:
+	 * Display the gradient image.
+	 *
+	 * @param f the graylevel image (row major representation)
+	 * @param width of the image
+	 * @param height of the image
+	 */
+	public void gradientImage(byte[] f, int width, int height) {
+
+	}
+
+    /**
+	 * Task 2:
+	 * Display the response map R of the harris corner detector. For simplicity, you may use a 5x5 box smoothing filter instead of Gaussian.
+	 * Set all pixels to white for which R > 100000.
+	 *
+	 * @param f the graylevel image (row major representation)
+	 * @param width of the image
+	 * @param height of the image
+	 */
+	public void cornerResponseImage(byte[] f, int width, int height) {
+	}
+}
